1b3474117433b34b4a5be7751cbf25aff8388976
Jonas I. Liechti <j-i-l@t4d.ch>
2024-04-11 22:45:38 +0200

    adapting to data in /shares/...

--- src/landiv_blur/scripts/console_launcher.sh
+++ src/landiv_blur/scripts/console_launcher.sh
@@ -6,4 +6,4 @@
 
 module load mamba
 source activate landiv
-srun python parallel_filter.py --source=/data/jliech/landiv/first_approach/Europe/landcover/reclass_GLC_FCS30_2015_utm32U.tif --output=/scratch/jliech/landiv/first_approach/Europe/output/lct_heterogeneity_diam_30000_utm32u.tif --scale=30 --diameter=30000 --truncate=3 --nbrcpu=64 --bwidth=2220 --bheight=2960
+srun python parallel_filter.py --source=/shares/niklaus.ieu.uzh/first_approach/Europe/landcover/reclass_GLC_FCS30_2015_utm32U.tif --output=/shares/niklaus.ieu.uzh/first_approach/Europe/output/lct_heterogeneity_diam_30000_utm32u.tif --scale=30 --diameter=30000 --truncate=3 --nbrcpu=64 --bwidth=2220 --bheight=2960
