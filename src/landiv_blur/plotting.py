1572918210f1ec43ece8fdfc40b8c3f5498048d5
Jonas Liechti <j-i-l@t4d.ch>
2024-04-29 19:25:59 +0200

    allow more flexible plotting

--- src/landiv_blur/plotting.py
+++ src/landiv_blur/plotting.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib.colors import ListedColormap, LinearSegmentedColormap
@@ -11,18 +12,18 @@ from .processing import (
 
 OUT = 'black'
 FOREST = 'darkgreen'
-WATER = 'lightblue'
+WATER = 'blue'
 URBAN = 'red'
 GRASSLAND = 'lightgreen'
 COLORS = [
-    OUT,
-    URBAN,
-    FOREST,
     GRASSLAND,
+    FOREST,
+    URBAN,
     'blue',
-    'purple',
+    'yellow',
     WATER,
-    'orange'
+    'purple',
+    'teal',
 ]
 DPI = 200
 
@@ -90,7 +91,7 @@ def plot_landtypes(source, start, size, output):
 def show_layer(data, layer, transform, ax):
     """
     """
-    colors = [COLORS[0], COLORS[layer-1]]
+    colors = [OUT, COLORS[layer-1]]
     if len(np.unique(data)) == 2:
         cmap = _get_class_colormap(colors=colors)
     else:
@@ -103,8 +104,10 @@ def show_layer(data, layer, transform, ax):
     )
 
 
-def plot_layers(source, start, size, output, img_filter=None, params=None):
-    """Plot 8 layers in isolation
+def plot_layers(source, start, size, img_filter=None, params=None,
+                scaling=None, layers=None,
+                fig_params=dict(), scaling_params=dict()):
+    """Plot each layer in isolation
 
     Parameters
     ----------
@@ -114,25 +117,68 @@ def plot_layers(source, start, size, output, img_filter=None, params=None):
       horizontal and vertical starting coordinate
     size: tuple
       width and height of the block to extract
-    output: str
-      Where to store the image
+    
+    keywords:
+      axs: 
+        Axes to draw on
+      gs:
+        GridSpec
+      output: str
+        Where to store the image
     """
-    block = load_block(source, start, size)
+    block = load_block(source, start, size, indexes=None, scaling=scaling,
+                       **scaling_params)
     data, transform = block['data'], block['transform']
 
-    fig, axs = plt.subplots(2, 4, figsize=(128, 64))
+    axs = fig_params.get('axs', None)
+    gs = fig_params.get('gs', None)
+    if axs is None and gs is None:
+        fig, axs = plt.subplots(2, 4, figsize=(128, 64))
 
-    for row in range(2):
-        for col in range(4):
+        def _get_axis(row, col):
+            return axs[row, col]
+    elif axs is None:
+        fig = fig_params.get('fig')
+
+        def _get_axis(row, col):
+            return fig.add_subplot(gs[row, col])
+    else:
+
+        def _get_axis(row, col):
+            return axs[row, col]
+
+    if not layers:
+        # TODO: use get_lct to get the number of layers
+        rows = range(2)
+        cols = range(4)
+    if len(layers) <= 4:
+        rows = range(1)
+        cols = range(len(layers))
+    else:
+        rows = range(2)
+        cols = range(math.ceil(0.5*len(layers)))
+    for row in rows:
+        for col in cols:
             print((col, row))
-            layer = 1 + col + row * 4
-            print(layer)
+            _layer = col + row * 4
+            if layers:
+                try:
+                    layer = layers[_layer]
+                except IndexError:
+                    # we plotted all layers
+                    continue
+            else:
+                layer = _layer
             _data = get_layer_data(data, layer, img_filter, params)
-            show_layer(_data, layer, transform, axs[row, col])
-    fig.savefig(output, dpi=DPI)
+            show_layer(_data, _layer+1, transform, _get_axis(row,  col))
+    plt.subplots_adjust(wspace=0.1, hspace=0.1)
+    output = fig_params.get('output', None)
+    if output:
+        fig.savefig(output, dpi=DPI)
+    return axs
 
 
-def plot_entropy(source, start, size, output):
+def plot_entropy(source, start, size, output=None, **fig_params):
     """Plot the entropy in each pixel from a tif file
 
     Parameters
@@ -147,7 +193,10 @@ def plot_entropy(source, start, size, output):
     block = load_block(source=source, start=start, size=size)
     data, transform = block['data'], block['transform']
     entropy_layer = data
-    fig, ax = plt.subplots(figsize=(16, 16))
+    ax = fig_params.get('ax', None)
+    fig = fig_params.get('fig', None)
+    if not ax:
+        fig, ax = plt.subplots(figsize=(16, 16))
 
     cmap = LinearSegmentedColormap.from_list(
         "Custom", ['black', 'white'], N=40)
@@ -157,12 +206,14 @@ def plot_entropy(source, start, size, output):
                       transform=transform,
                       cmap=cmap)
     im = to_display.get_images()[0]
-    fig.colorbar(im, ax=ax)
-    fig.savefig(output, dpi=DPI)
+    if fig:
+        fig.colorbar(im, ax=ax)
+        fig.savefig(output, dpi=DPI)
+    return ax
 
 
-def plot_entropy_full(source, start, size, output, layers=None,
-                      img_filter=None, params=None):
+def plot_entropy_full(source, start, size, output, img_filter=None,
+                      params=None):
     """Plot the entropy in each pixel after layer diffusion
 
     Parameters
@@ -175,14 +226,10 @@ def plot_entropy_full(source, start, size, output, layers=None,
       width and height of the block to extract
     output: str
       Where to store the image
-    layers: iterable or None
-      A list or other iterable with the land-cover types to include.
-      If not provided then all land-cover types are included
     """
     block = load_block(source, start, size)
     data, transform = block['data'], block['transform']
-    entropy_layer = get_entropy(data, layers=layers, normed=True,
-                                dtype=np.uint8)
+    entropy_layer = get_entropy(data, layers=range(8), normed=True)
 
     fig, ax = plt.subplots(figsize=(16, 16))
 
