94b278e6f9066ab4c65c9754a25c3645164f3b32
Jonas Liechti <j-i-l@t4d.ch>
2024-04-29 19:26:35 +0200

    wip - plotting utm layers and entropy

---
+++ examples/present_utm.py
@@ -0,0 +1,72 @@
+import os
+from rasterio.enums import Resampling
+from matplotlib import pyplot as plt
+import matplotlib.gridspec as gridspec
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+
+from landiv_blur import io as lbio
+from landiv_blur import prepare as lbprep
+from landiv_blur import plotting as lbplot
+
+utm_zone = 'utm32U'
+# we are going to re-scale the maps by changing the resolution by this factor:
+scaling = 0.5
+# the land-cover types to plot
+lc_types = [4, 2, 9, 10]
+
+
+# this is going to be our figure
+fig = plt.figure(tight_layout=True, figsize=(128, 128))
+gs = gridspec.GridSpec(4, 4)
+
+# fig, axs = plt.subplots(2, 4, figsize=(128, 128))
+
+
+# first we get the individual layers from the source file
+data_path = '../data/landcover'
+filename = 'reclass_GLC_FCS30_2015_{utm_zone}.tif'
+source_file = os.path.join(data_path, filename.format(utm_zone=utm_zone))
+# for the layers we increase visibility:
+s_method_layer = Resampling.nearest
+
+# to plot the layers individually we have a little helper function
+_axs = lbplot.plot_layers(source_file, None, None, scaling=scaling,
+                          scaling_params=dict(scaling_method=s_method_layer),
+                          # see #31 for the layer combination
+                          layers=lc_types, fig_params=dict(fig=fig, gs=gs))
+                          # layers=[[1, 4], 2, [8, 9], 10], axs=axs)
+
+# now we load the blurred layers
+result_map = 'lct_heterogeneity_{utm_zone}_{res_type}_sig_{sigma}_diam_{diameter}_trunc_{truncate}.tif'
+results_path = '../results/'
+diameter = 1000.
+truncate = 3.
+scaling_method = Resampling.nearest
+blur_params = lbprep.get_blur_params(diameter=diameter, truncate=truncate)
+blurred_map = result_map.format(utm_zone=utm_zone.lower(),
+                                res_type='lct_blurred',
+                                **blur_params)
+source_file = os.path.join(results_path, blurred_map)
+
+for i, lct in enumerate(lc_types):
+    ax = fig.add_subplot(gs[1, i])
+    utm_map = lbio.load_block(source_file,
+                              indexes=lct,
+                              )
+                              # scaling=scaling,
+                              # scaling_method=scaling_method)
+    # encoded the layers starting from 0
+    lbplot.show_layer(utm_map['data'], layer=i-1, transform=utm_map['transform'],
+                      ax=ax)
+
+# now we get the entropy map and plot it
+entropy_map = result_map.format(utm_zone=utm_zone.lower(),
+                                res_type='entropy',
+                                **blur_params)
+source_file = os.path.join(results_path, entropy_map)
+
+ax = fig.add_subplot(gs[2:, 1:3])
+lbplot.plot_entropy(source=source_file, size=None, start=None, fig=fig, ax=ax)
+# finally we save it
+fig.savefig('utm_32u.pdf')
+#fig.savefig('single_lct.pdf', dpi=lbplot.DPI)
