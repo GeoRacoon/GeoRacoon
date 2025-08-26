91879a7cf9ce8e03363954862c28485774a3e632
Jonas I. Liechti <j-i-l@t4d.ch>
2025-08-26 16:14:06 +0200

    ready smoke tests for correct installation

---
+++ src/linfit/__init__.py
@@ -0,0 +1,5 @@
+"""
+"""
+
+answer_to_everything = 42
+
---
+++ tests/test_linfit/test_linfit_init.py
@@ -0,0 +1,12 @@
+import pytest
+
+import linfit as lft
+
+def test_package_import():
+    """
+    Simple test to verify package import
+    """
+    assert lft.answer_to_everything == 42
+
+
+
