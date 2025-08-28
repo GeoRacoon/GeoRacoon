a0820e36efed8ca60c5d1695ac838c6c9425f0f4
Jonas I. Liechti <j-i-l@t4d.ch>
2025-08-28 16:32:48 +0200

    init destination

---
+++ src/riogrande/io_.py
@@ -0,0 +1,39 @@
+from __future__ import annotations
+import os
+
+import numpy as np
+import rasterio as rio
+
+from rasterio.windows import Window
+from contextlib import contextmanager
+from dataclasses import dataclass, field
+from pathlib import Path
+from functools import partial
+from numpy.typing import NDArray
+from collections.abc import Callable
+
+from typing import Union
+
+from ._exceptions import (
+    BandSelectionNoMatchError,
+    BandSelectionAmbiguousError,
+    SourceNotSavedError,
+    UnknownExtensionError,
+    InvalidMaskSelectorError,
+)
+
+from .io import (
+    NS,
+    get_tags,
+    set_tags,
+    get_bidx,
+    match_all,
+    find_bidxs,
+    compress_tif,
+    load_block
+)
+from ._helper import (
+    check_compatibility as _check_compatibility,
+    count_contribution,
+)
+
