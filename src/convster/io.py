70ae5bc904ef198217ea6c732024fdb478b7a8d7
Jonas I. Liechti <j-i-l@t4d.ch>
2025-08-29 16:34:16 +0200

    cleaning out unused dependencies

--- src/convster/io.py
+++ src/convster/io.py
@@ -1,47 +1,17 @@
 from __future__ import annotations
 
-import os
-import glob
 import rasterio
 
 
-from math import floor
-
-from typing import Any
 
 import rasterio as rio
-from rasterio.io import DatasetWriter
-from rasterio.enums import ColorInterp
-from rasterio.windows import Window
-from rasterio.enums import Resampling
 from rasterio.mask import mask
-from rasterio.warp import (
-    calculate_default_transform,
-    reproject,
-    Resampling,
-    transform_bounds
-)
 
 from shapely.geometry import box as shbox
 import geopandas as gpd
 
-from numpy.typing import NDArray
-
-from ._exceptions import (
-    BandSelectionNoMatchError,
-    BandSelectionAmbiguousError,
-    SourceNotSavedError,
-    UnknownExtensionError,
-)
-from ._helper import (
-    check_crs_raster,
+from .helper import (
     outfile_suffix,
-    serialize,
-    deserialize,
-    sanitize,
-    match_all,
-    view_to_window,
-    get_scale_factor,
 )
 # is_needed
 # needs_work (module should be moved, needs docs)
