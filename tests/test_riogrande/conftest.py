9f938f6ce4d85bbbb3390bbbe4be3e2f8399d16a
Jonas I. Liechti <j-i-l@t4d.ch>
2025-08-29 19:18:18 +0200

    done with imports in conftest files

--- tests/test_riogrande/conftest.py
+++ tests/test_riogrande/conftest.py
@@ -5,7 +5,7 @@ import glob
 import numpy as np
 from rasterio.transform import Affine
 
-from landiv_blur.helper import get_or_set_context
+from riogrande.helper import get_or_set_context
 
 FIXTURE_DIR = os.path.abspath(os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
