4555ea53fb17e2b749a89d10ffe9f7c9b87c8b9c
simonsaysenjoy <simon.landauer@gmail.com>
2025-09-24 14:33:58 +0200

    helper docs and tests

--- src/convster/helper.py
+++ src/convster/helper.py
@@ -3,28 +3,79 @@ This module contains various helper functions to process numpy arrays
 """
 
 import numpy as np
+from numpy.typing import NDArray
 
 
-def first_nonzero(data, axis=0, no_value=-1):
-    # TODO: is_needed - needs_work - is_tested - usedin_processing
-    """Return the first non-zero value along an axis
+def first_nonzero(data: NDArray , axis: int = 0, no_value: int = -1) -> NDArray:
     """
-    # is_needed
-    # usedin_processing
-    # no_work
-    # not_tested (used in other test)
+    Return the index of the first non-zero value along the given axis.
+
+    Parameters
+    ----------
+    data
+        Input array to examine.
+    axis
+        Array axis along which to search for the first non-zero. Default is 0.
+    no_value
+        Value to return when no non-zero entries are found along an axis.
+        Default is -1.
+
+    Returns
+    -------
+    indices
+        Array indices of the first non-zero values along the specified axis.
+        If no non-zero is found, returns `no_value` for that slice.
+
+    Examples
+    --------
+    >>> a = np.array([
+    ...     [0, 0, 3, 0],
+    ...     [1, 0, 0, 0],
+    ...     [0, 2, 0, 0],
+    ...     [0, 0, 0, 4]
+    ... ])
+    >>> first_nonzero(a, axis=1)
+    array([2, 0, 1, 3])
+    >>> first_nonzero(a, axis=0)
+    array([1, 2, 0, 3])
+    """
     mask = data != 0
     return np.where(mask.any(axis=axis), mask.argmax(axis=axis), no_value)
 
 
 def last_nonzero(data, axis=0, no_value=-1):
-    # TODO: is_needed - needs_work - is_tested - usedin_processing
-    """Return the last non-zero value along an axis
     """
-    # is_needed
-    # usedin_processing
-    # no_work
-    # not_tested (used in other test)
+    Return the index of the last non-zero value along the given axis.
+
+    Parameters
+    ----------
+    data
+     Input array to examine.
+    axis
+     Array axis along which to search for the last non-zero. Default is 0.
+    no_value
+     Value to return when no non-zero entries are found along an axis.
+     Default is -1.
+
+    Returns
+    -------
+    indices
+     Array indices of the last non-zero values along the specified axis.
+     If no non-zero is found, returns `no_value` for that slice.
+
+    Examples
+    --------
+    >>> a = np.array([
+    ...     [0, 0, 3, 0],
+    ...     [1, 0, 0, 0],
+    ...     [0, 2, 0, 0],
+    ...     [0, 0, 0, 4]
+    ... ])
+    >>> last_nonzero(a, axis=1)
+    array([2, 0, 1, 3])
+    >>> last_nonzero(a, axis=0)
+    array([1, 2, 2, 3])
+    """
     mask = data != 0
     loc = data.shape[axis] - np.flip(mask, axis=axis).argmax(axis=axis) - 1
     return np.where(mask.any(axis=axis), loc, no_value)
