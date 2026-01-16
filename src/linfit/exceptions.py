cb66564c7433d2e487cfd749f2c25d908e02540a
simonsaysenjoy <simon.landauer@gmail.com>
2026-01-16 12:50:57 +0100

    deleted todos

--- src/linfit/exceptions.py
+++ src/linfit/exceptions.py
@@ -5,18 +5,10 @@
 class InvalidPredictorError(Exception):
     """Not all the provided predictors are valid.
     """
-    # is_needed
-    # no_work
-    # not_tested (no need)
-    # usedin_linfit
     pass
 
 
 class InferenceError(Exception):
     """Some steps of a multiple linear regression have invalid parameters
     """
-    # is_needed
-    # no_work
-    # not_tested (no need)
-    # usedin_linfit
     pass
