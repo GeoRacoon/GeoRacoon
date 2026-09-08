dee578134ebca340a9133f25b6bfb23099f81b92
simonlandauer <simon.landauer@gmail.com>
2026-09-08 10:39:54 +0200

    first batch of licenses added

--- data/fetch.py
+++ data/fetch.py
@@ -1,3 +1,8 @@
+# SPDX-FileCopyrightText: 2026 Jonas I. Liechti <j-i-l@t4d.ch>
+# SPDX-FileCopyrightText: 2026 Simon Landauer <georacccoon@proton.me>
+#
+# SPDX-License-Identifier: MIT
+
 """Fetch GeoRacoon's example and test raster fixtures from Zenodo.
 
 The rasters used by ``examples/`` and ``tests/`` are not shipped in this git
--- docs/content/georacoon/installation.md
+++ docs/content/georacoon/installation.md
@@ -1,3 +1,10 @@
+<!--
+SPDX-FileCopyrightText: 2026 Jonas I. Liechti <j-i-l@t4d.ch>
+SPDX-FileCopyrightText: 2026 Simon Landauer <georacccoon@proton.me>
+
+SPDX-License-Identifier: MIT
+-->
+
 # Installation
 
 ```{include} ../../../README.md
