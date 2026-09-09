eb3412b0c21bb286d6e792e81d02e6e44a365d21
Jonas I. Liechti <j-i-l@t4d.ch>
2026-09-09 14:09:55 +0200

    more spdx

--- docs/benchmarks/plot_parallelization_tradeoffs.py
+++ docs/benchmarks/plot_parallelization_tradeoffs.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+# SPDX-FileCopyrightText: 2026 Jonas I. Liechti <j-i-l@t4d.ch>
+# SPDX-FileCopyrightText: 2026 Simon Landauer <georacccoon@proton.me>
+#
+# SPDX-License-Identifier: MIT
 """
 The *coon-way*: Fragmented raster computations
 ==============================================
